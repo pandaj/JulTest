44caa5aa4e353a1a9157d6852f1cb83bc1cd44a0
pandaj <mail@juliocofre.cl>
2013-08-01 19:52:43 -0400

    Anima Extra
    assdas

---
+++ js/animacion.js
@@ -0,0 +1,55 @@
+// JavaScript Document
+
+var escena = document.getElementById("escena");
+		if (escena.getContext) {
+			
+			
+			/********* CARGA SPRITES *********/
+			var posX = 20; //posicion absoluta en X
+			var posY = 20; // posicion absoluta en Y
+			var width = 44; // ancho de cada sprite
+			var height = 63; // alto de cada sprite
+			var indica = 0; // indicador del sprite actual
+			var tope = 6; // cantidad total de sprites - 1
+			
+			var contenido = escena.getContext('2d');
+			var sprites = new Image();
+			sprites.src = "images/kim_anima.png";
+			function cambiaSprite() {
+				contenido.drawImage(sprites, (width*indica), 0, width, height, posX, posY, width, height);
+			}
+			sprites.onload = function() {
+				limpieza();
+				cambiaSprite();
+				animaCirculo = setInterval(animaC, vel);
+			}
+			
+			var animaCirculo;
+			var vel = 100; // velocidad de animacion
+			function limpieza() {
+				contenido.clearRect(posX, posY, width, height);
+			}
+			
+			function animaC() {
+				if (indica == tope) {
+					indica = 0;
+				} else {
+					indica += 1;
+				}
+				clearInterval(animaCirculo);
+				animaCirculo = setInterval(animaC, vel);
+				limpieza();
+				cambiaSprite();
+			}
+			
+			/************** APURAR / RETRASAR *********/
+			
+			function apurar() {
+				if (vel > 10) {
+					vel -= 10;
+				}
+			}
+			function retrasar() {
+				vel += 10;
+			}
+		}
