e7c639f475b7a5dcc401cbe85d61633b08733d39
pandaj <mail@juliocofre.cl>
2013-08-09 20:09:02 -0400

    Disgaea 0.2
    Eso

---
+++ js/disgaeaBD.js
@@ -0,0 +1,65 @@
+// JavaScript Document
+
+/*----- DEMI-HUMANS -----*/
+
+var demiHumans = new Array();
+
+demiHumans[0] = new Object();
+demiHumans[0].nombre = 'Archer';
+
+demiHumans[1] = new Object();
+demiHumans[1].nombre = 'Beastmaster';
+
+demiHumans[2] = new Object();
+demiHumans[2].nombre = 'Berserker';
+
+demiHumans[3] = new Object();
+demiHumans[3].nombre = 'Cheerleader';
+
+demiHumans[4] = new Object();
+demiHumans[4].nombre = 'Female_Brawler';
+
+demiHumans[5] = new Object();
+demiHumans[5].nombre = 'Female_Fighter';
+
+demiHumans[6] = new Object();
+demiHumans[6].nombre = 'Female_Healer';
+
+demiHumans[7] = new Object();
+demiHumans[7].nombre = 'Female_Samurai';
+
+demiHumans[8] = new Object();
+demiHumans[8].nombre = 'Geomancer';
+
+demiHumans[9] = new Object();
+demiHumans[9].nombre = 'Gunner';
+
+demiHumans[10] = new Object();
+demiHumans[10].nombre = 'Gunslinger';
+
+demiHumans[11] = new Object();
+demiHumans[11].nombre = 'Heavy_Knight';
+
+demiHumans[12] = new Object();
+demiHumans[12].nombre = 'Kunoichi';
+
+demiHumans[13] = new Object();
+demiHumans[13].nombre = 'Mage';
+
+demiHumans[14] = new Object();
+demiHumans[14].nombre = '';
+
+demiHumans[15] = new Object();
+demiHumans[15].nombre = '';
+
+demiHumans[16] = new Object();
+demiHumans[16].nombre = '';
+
+demiHumans[17] = new Object();
+demiHumans[17].nombre = '';
+
+demiHumans[18] = new Object();
+demiHumans[18].nombre = '';
+
+demiHumans[19] = new Object();
+demiHumans[19].nombre = '';
