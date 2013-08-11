413eec71fce45fa0b3b280c2d8410497505b1200
pandaj <mail@juliocofre.cl>
2013-08-11 02:34:20 -0400

    Disgaea 0.4
    Eso

--- js/disgaea.js
+++ js/disgaea.js
@@ -1,5 +1,36 @@
 // JavaScript Document
 
 window.onload = function(){
-	
+	for (var d = 0; d < demiHumans.length; d++) {
+		var nombreD = demiHumans[d].nombre.split("_");
+		nombreD = nombreD.join(" ");
+		
+		$('#select-char').append(
+			'<div id="' + demiHumans[d].nombre + '" class="char-face">' +
+				'<a class="char-boton" href="#">' + 
+					'<img src="' + 'disgaea/faces/D_' + demiHumans[d].nombre + '.jpg' + '" />' + 
+					'<p>' + nombreD + '</p>' + 
+				'</a>' + 
+			'</div>'
+		);
+		botonChar(d, demiHumans);
+	}
+	for (var m = 0; m < monsters.length; m++) {
+		var nombreM = monsters[m].nombre.split("_");
+		nombreM = nombreM.join(" ");
+		
+		$('#select-char').append(
+			'<div id="' + monsters[m].nombre + '" class="char-face">' +
+				'<a class="char-boton" href="#">' + 
+					'<img src="' + 'disgaea/faces/M_' + monsters[m].nombre + '.jpg' + '" />' + 
+					'<p>' + nombreM + '</p>' + 
+				'</a>' + 
+			'</div>'
+		);
+		botonChar(m, monsters);
+	}
+}
+
+function botonChar(num, lista) {
+	//$('#' + lista[num].nombre)
 }
--- js/disgaeaBD.js
+++ js/disgaeaBD.js
@@ -1,6 +1,8 @@
 // JavaScript Document
 
+
 /*----- DEMI-HUMANS -----*/
+
 
 var demiHumans = new Array();
 
@@ -18,6 +20,42 @@ demiHumans[3].nombre = 'Cheerleader';
 
 demiHumans[4] = new Object();
 demiHumans[4].nombre = 'Female_Brawler';
+demiHumans[4].clases = new Array();
+demiHumans[4].clases[0] = new Object();
+demiHumans[4].clases[0].nombre = 'Wind Spinner';
+demiHumans[4].clases[0].requiere = 'Inicial';
+demiHumans[4].clases[1] = new Object();
+demiHumans[4].clases[1].nombre = 'Thunder Fist';
+demiHumans[4].clases[1].requiere = 'Wind Spinner Nv 20';
+demiHumans[4].clases[2] = new Object();
+demiHumans[4].clases[2].nombre = 'Storm Bringer';
+demiHumans[4].clases[2].requiere = 'Thunder Fist Nv 40';
+demiHumans[4].clases[3] = new Object();
+demiHumans[4].clases[3].nombre = 'Sky Faller';
+demiHumans[4].clases[3].requiere = 'Storm Bringer Nv 80';
+demiHumans[4].clases[4] = new Object();
+demiHumans[4].clases[4].nombre = 'Star Warrior';
+demiHumans[4].clases[4].requiere = 'Sky Faller Nv 160';
+demiHumans[4].clases[5] = new Object();
+demiHumans[4].clases[5].nombre = 'God Buster';
+demiHumans[4].clases[5].requiere = 'Star Warrior Nv 360';
+demiHumans[4].armas = new Array();
+demiHumans[4].armas[0] = 'Nudillos Nv 6';
+demiHumans[4].armas[1] = 'Espada Nv 4';
+demiHumans[4].skills = new Array();
+demiHumans[4].skills[0] = 'Triple Fist';
+demiHumans[4].skills[1] = 'Phantom Fist';
+demiHumans[4].skills[2] = 'Blazing Palm';
+demiHumans[4].unSkills = new Array();
+demiHumans[4].unSkills[0] = 'Auto-Recovery';
+demiHumans[4].unSkills[1] = 'Absolute Combo';
+demiHumans[4].evilities = new Array();
+demiHumans[4].evilities[0] = 'Heal Gain';
+demiHumans[4].unEvilities = new Array();
+demiHumans[4].unEvilities[0] = 'Feint';
+demiHumans[4].unEvilities[1] = 'Impromptu';
+demiHumans[4].unEvilities[2] = 'Counter Chance';
+demiHumans[4].unEvilities[3] = 'Hurt Me, Kill You';
 
 demiHumans[5] = new Object();
 demiHumans[5].nombre = 'Female_Fighter';
@@ -81,3 +119,81 @@ demiHumans[24].nombre = 'Skull';
 
 demiHumans[25] = new Object();
 demiHumans[25].nombre = 'Thief';
+
+demiHumans[26] = new Object();
+demiHumans[26].nombre = 'Angel';
+
+
+/*----- MONSTER -----*/
+
+
+var monsters = new Array();
+
+monsters[0] = new Object();
+monsters[0].nombre = 'Alraune';
+
+monsters[1] = new Object();
+monsters[1].nombre = 'Catsaber';
+
+monsters[2] = new Object();
+monsters[2].nombre = 'Cockatrice';
+
+monsters[3] = new Object();
+monsters[3].nombre = 'Dragon_Zombie';
+
+monsters[4] = new Object();
+monsters[4].nombre = 'Eryngi';
+
+monsters[5] = new Object();
+monsters[5].nombre = 'Ghost';
+
+monsters[6] = new Object();
+monsters[6].nombre = 'Holy_Dragon';
+
+monsters[7] = new Object();
+monsters[7].nombre = 'Ifrit';
+
+monsters[8] = new Object();
+monsters[8].nombre = 'Marionette';
+
+monsters[9] = new Object();
+monsters[9].nombre = 'Mothman';
+
+monsters[10] = new Object();
+monsters[10].nombre = 'Mystic_Beast';
+
+monsters[11] = new Object();
+monsters[11].nombre = 'Orc';
+
+monsters[12] = new Object();
+monsters[12].nombre = 'Prinny';
+
+monsters[13] = new Object();
+monsters[13].nombre = 'Reaper';
+
+monsters[14] = new Object();
+monsters[14].nombre = 'Rifle_Demon';
+
+monsters[15] = new Object();
+monsters[15].nombre = 'Slime';
+
+monsters[16] = new Object();
+monsters[16].nombre = 'Succubus';
+
+monsters[17] = new Object();
+monsters[17].nombre = 'Warslug';
+
+monsters[18] = new Object();
+monsters[18].nombre = 'Wood_Golem';
+
+monsters[19] = new Object();
+monsters[19].nombre = 'Zombie';
+
+monsters[20] = new Object();
+monsters[20].nombre = 'Nekomata';
+
+monsters[21] = new Object();
+monsters[21].nombre = 'Prism_Ranger';
+
+monsters[22] = new Object();
+monsters[22].nombre = 'Dragon';
