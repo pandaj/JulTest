f3a4f44e02096ef7cb729b50ca79c5ef5d3fee98
pandaj <mail@juliocofre.cl>
2013-08-27 11:00:43 -0400

    Prueba Colores
    eso

---
+++ paletaColores.js
@@ -0,0 +1,147 @@
+//PALETAS DE COLORES DE PELOS
+
+	
+var colorPelo = new Array();
+
+//Pelo Verde
+colorPelo[0] = new Array();
+colorPelo[0][0] = new Array(165, 196, 124);
+colorPelo[0][1] = new Array(148, 176, 109);
+colorPelo[0][2] = new Array(126, 152, 94);
+colorPelo[0][3] = new Array(105, 128, 79);
+colorPelo[0][4] = new Array(81, 98, 66);
+colorPelo[0][5] = new Array(58, 69, 53);
+
+//Pelo Rojo
+colorPelo[1] = new Array();
+colorPelo[1][0] = new Array(203, 110, 100);
+colorPelo[1][1] = new Array(181, 93, 89);
+colorPelo[1][2] = new Array(160, 76, 78);
+colorPelo[1][3] = new Array(132, 60, 71);
+colorPelo[1][4] = new Array(104, 50, 59);
+colorPelo[1][5] = new Array(77, 40, 48);
+
+//Pelo Amarillo
+colorPelo[2] = new Array();
+colorPelo[2][0] = new Array(244, 223, 184);
+colorPelo[2][1] = new Array(221, 193, 156);
+colorPelo[2][2] = new Array(198, 163, 128);
+colorPelo[2][3] = new Array(176, 133, 100);
+colorPelo[2][4] = new Array(133, 93, 76);
+colorPelo[2][5] = new Array(90, 53, 53);
+
+//Pelo Morado
+colorPelo[3] = new Array();
+colorPelo[3][0] = new Array(171, 139, 182);
+colorPelo[3][1] = new Array(150, 115, 159);
+colorPelo[3][2] = new Array(130, 92, 136);
+colorPelo[3][3] = new Array(110, 69, 114);
+colorPelo[3][4] = new Array(87, 56, 86);
+colorPelo[3][5] = new Array(64, 43, 59);
+
+//Pelo Azul
+colorPelo[4] = new Array();
+colorPelo[4][0] = new Array(128, 156, 205);
+colorPelo[4][1] = new Array(112, 137, 185);
+colorPelo[4][2] = new Array(97, 119, 165);
+colorPelo[4][3] = new Array(82, 101, 145);
+colorPelo[4][4] = new Array(69, 68, 116);
+colorPelo[4][5] = new Array(56, 43, 88);
+
+//Pelo Celeste
+colorPelo[5] = new Array();
+colorPelo[5][0] = new Array(168, 192, 200);
+colorPelo[5][1] = new Array(146, 164, 172);
+colorPelo[5][2] = new Array(124, 136, 144);
+colorPelo[5][3] = new Array(102, 108, 116);
+colorPelo[5][4] = new Array(80, 80, 88);
+colorPelo[5][5] = new Array(58, 53, 61);
+
+//Pelo Negro
+colorPelo[6] = new Array();
+colorPelo[6][0] = new Array(146, 140, 143);
+colorPelo[6][1] = new Array(127, 121, 125);
+colorPelo[6][2] = new Array(109, 102, 107);
+colorPelo[6][3] = new Array(91, 83, 90);
+colorPelo[6][4] = new Array(73, 64, 72);
+colorPelo[6][5] = new Array(55, 45, 55);
+
+
+//PALETAS DE COLORES DE ARMADURA
+
+
+var colorArmor = new Array();
+
+//Armadura Ploma
+colorArmor[0] = new Array();
+colorArmor[0][0] = new Array(243, 244, 244);
+colorArmor[0][1] = new Array(177, 188, 193);
+colorArmor[0][2] = new Array(149, 158, 166);
+colorArmor[0][3] = new Array(121, 128, 140);
+colorArmor[0][4] = new Array(93, 99, 114);
+colorArmor[0][5] = new Array(87, 88, 106);
+
+//Armadura Verde
+colorArmor[1] = new Array();
+colorArmor[1][0] = new Array(250, 242, 217);
+colorArmor[1][1] = new Array(189, 185, 151);
+colorArmor[1][2] = new Array(162, 154, 116);
+colorArmor[1][3] = new Array(136, 125, 92);
+colorArmor[1][4] = new Array(111, 97, 69);
+colorArmor[1][5] = new Array(102, 88, 74);
+
+//Armadura Amarilla
+colorArmor[2] = new Array();
+colorArmor[2][0] = new Array(255, 250, 220);
+colorArmor[2][1] = new Array(241, 205, 153);
+colorArmor[2][2] = new Array(208, 154, 117);
+colorArmor[2][3] = new Array(172, 123, 98);
+colorArmor[2][4] = new Array(137, 93, 79);
+colorArmor[2][5] = new Array(140, 77, 71);
+
+//Armadura Roja
+colorArmor[3] = new Array();
+colorArmor[3][0] = new Array(248, 230, 212);
+colorArmor[3][1] = new Array(202, 158, 148);
+colorArmor[3][2] = new Array(181, 136, 122);
+colorArmor[3][3] = new Array(153, 110, 101);
+colorArmor[3][4] = new Array(126, 84, 80);
+colorArmor[3][5] = new Array(109, 70, 80);
+
+//Armadura Rosada
+colorArmor[4] = new Array();
+colorArmor[4][0] = new Array(237, 235, 225);
+colorArmor[4][1] = new Array(203, 175, 197);
+colorArmor[4][2] = new Array(170, 143, 160);
+colorArmor[4][3] = new Array(138, 112, 124);
+colorArmor[4][4] = new Array(118, 89, 108);
+colorArmor[4][5] = new Array(98, 67, 93);
+
+//Armadura Morada
+colorArmor[5] = new Array();
+colorArmor[5][0] = new Array(211, 209, 215);
+colorArmor[5][1] = new Array(139, 127, 165);
+colorArmor[5][2] = new Array(110, 99, 132);
+colorArmor[5][3] = new Array(82, 72, 100);
+colorArmor[5][4] = new Array(73, 60, 83);
+colorArmor[5][5] = new Array(64, 49, 67);
+
+//Armadura Negra
+colorArmor[6] = new Array();
+colorArmor[6][0] = new Array(169, 176, 179);
+colorArmor[6][1] = new Array(131, 131, 136);
+colorArmor[6][2] = new Array(104, 101, 108);
+colorArmor[6][3] = new Array(77, 71, 81);
+colorArmor[6][4] = new Array(71, 64, 74);
+colorArmor[6][5] = new Array(63, 56, 66);
+
+
+
+//Piel
+/*
+254, 228, 205
+242, 208, 184
+224, 183, 156
+203, 156, 131
+183, 129, 107
+*/
